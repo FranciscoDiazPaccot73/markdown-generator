780d72811d6013bbaea56db7be8ad9359db8e21b
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2022-10-21 15:09:02 -0300

    fixes

--- utils/index.js
+++ utils/index.js
@@ -59,7 +59,7 @@ export const META_TAGS = {
 	url: 'https://markdown-generator.vercel.app'
 }
 
-export const SAMPLE_TEXT = '# Markdown h1\n## Markdown h2 \n \n Simple plain text.\n\n Leave two spaces at the end of a line to go to the line.\n\n Attributs: *italic*, **bold** \n\n ## Make:\n\n * a\n * list\n\n Link with placeholder text: *[Medium](https://www.medium.com)* \n\n To use a simple link, just type it: https://www.medium.com/ \n\n ## Some extra documentation: \n\n > [MARKDOWN BASICS](https://www.markdownguide.org/basic-syntax/) \n \n > [Customize Markdown](https://help.start.gg/en/articles/1987102-customizing-text-with-markdown) ';
+export const SAMPLE_TEXT = '# <div align="center">Markdown h1</div>\n## Markdown h2 \n \n Simple plain text.\n\n Leave two spaces at the end of a line to go to the line.\n\n Attributs: *italic*, **bold** \n\n ## Make:\n\n * a\n * list\n\n Link with placeholder text: *[Medium](https://www.medium.com)* \n\n To use a simple link, just type it: https://www.medium.com/ \n\n\n You can also use common HTML language: \n\n > `<div align="center">` \n\n ## Some extra documentation: \n\n > [MARKDOWN BASICS](https://www.markdownguide.org/basic-syntax/) \n \n > [Customize Markdown](https://help.start.gg/en/articles/1987102-customizing-text-with-markdown) ';
 
 export const addTextFormat = (element, format) => {
 	const x = element.value;
